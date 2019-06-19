aa940c4f978c4ac8679ceb31b7c8a6d79a335f40
yang <1351599172@qq.com>
2019-06-19 10:48:18 +0800

    add model

--- controller.go
+++ controller.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"sync"
 	"time"
@@ -13,23 +12,17 @@ import (
 )
 
 var (
-	yuque = make(map[string]string)
-
 	// listRecode
 	lr sync.Map
-
 	// detailRecode
 	dr sync.Map
-
 	// groupRecode
 	gr sync.Map
 
 	// ListTime -
 	ListTime time.Time
-
 	// DetailTime -
 	DetailTime time.Time
-
 	// GroupTime -
 	GroupTime time.Time
 )
@@ -48,6 +41,8 @@ func getList(c *gin.Context) {
 		RepoID string `json:"repoid"`
 	}
 
+	var RepoResp ListRespon
+
 	ListNow := time.Now()
 	interval := ListNow.Sub(ListTime)
 	timer, _ := time.ParseDuration("24h")
@@ -59,38 +54,38 @@ func getList(c *gin.Context) {
 		return
 	}
 
-	url := "http://127.0.0.1:9090/list?RepoID=" + list.RepoID
+	url := fmt.Sprintf(ListURL, list.RepoID)
 
 	val, ok := lr.Load(list.RepoID)
 	if ok {
 		if interval > timer {
-			newbody, err := callAPI(c, url)
+			err := callAPI(c, url, &RepoResp)
 			if err != nil {
 				c.Error(err)
-				c.JSON(http.StatusBadGateway, gin.H{"status": http.StatusBadGateway})
+				c.JSON(http.StatusBadGateway, gin.H{"status": http.StatusBadGateway, "lists": val})
 				return
 			}
 
-			lr.Store(list.RepoID, string(newbody))
+			lr.Store(list.RepoID, RepoResp)
 			ListTime = time.Now()
-			c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "List": string(newbody)})
+			c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "lists": RepoResp})
 			return
 		}
 
-		c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "List": val})
+		c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "lists": val})
 		return
 	}
 
-	body, err := callAPI(c, url)
+	err = callAPI(c, url, &RepoResp)
 	if err != nil {
 		c.Error(err)
 		c.JSON(http.StatusForbidden, gin.H{"status": http.StatusForbidden})
 		return
 	}
 
-	lr.Store(list.RepoID, string(body))
+	lr.Store(list.RepoID, RepoResp)
 	ListTime = time.Now()
-	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "List": string(body)})
+	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "lists": RepoResp})
 }
 
 func getDetails(c *gin.Context) {
@@ -101,9 +96,11 @@ func getDetails(c *gin.Context) {
 		}
 	)
 
+	var DeResp DetailRespon
+
 	DetailNow := time.Now()
 	interval := DetailNow.Sub(DetailTime)
-	timer, _ := time.ParseDuration("24h")
+	timer, _ := time.ParseDuration("10s")
 
 	err := c.ShouldBind(&detail)
 	if err != nil {
@@ -112,43 +109,49 @@ func getDetails(c *gin.Context) {
 		return
 	}
 
-	url := "http://127.0.0.1:9090/detail?RepoID=" + detail.RepoID + "&ID=" + detail.ID
+	url := fmt.Sprintf(DetailURL, detail.RepoID, detail.ID)
 
-	val, ok := dr.Load(detail.RepoID)
-	fmt.Println(ok, "ppppp")
+	val, ok := dr.Load(detail.ID)
 	if ok {
 		if interval > timer {
-			newbody, err := callAPI(c, url)
+			err := callAPI(c, url, &DeResp)
 			if err != nil {
 				c.Error(err)
-				c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "List": val})
+				c.JSON(http.StatusBadGateway, gin.H{"status": http.StatusBadGateway, "details": val})
 				return
 			}
 
-			handleDetail(newbody, detail.RepoID)
-			c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "List": yuque})
+			dr.Store(detail.ID, DeResp)
+			DetailTime = time.Now()
+
+			c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "details": DeResp})
 			return
 		}
-		c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "List": val})
+		c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "details": val})
 		return
 	}
 
-	body, err := callAPI(c, url)
+	err = callAPI(c, url, &DeResp)
 	if err != nil {
 		c.Error(err)
 		c.JSON(http.StatusBadGateway, gin.H{"status": http.StatusBadGateway})
 		return
 	}
 
-	handleDetail(body, detail.RepoID)
-	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "Detail": yuque})
+	dr.Store(detail.ID, DeResp)
+	DetailTime = time.Now()
+	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "details": DeResp})
 
 }
 func getRepo(c *gin.Context) {
-	var Group struct {
-		GroupID  string `json:"groupid"`
-		RepoName string `json:"reponame"`
-	}
+	var (
+		Group struct {
+			GroupID  string `json:"groupid"`
+			RepoName string `json:"reponame"`
+		}
+
+		Repo RepoResp
+	)
 
 	GroupNow := time.Now()
 	interval := GroupNow.Sub(GroupTime)
@@ -161,46 +164,46 @@ func getRepo(c *gin.Context) {
 		return
 	}
 
-	url := "http://127.0.0.1:9090/groups?GroupID=" + Group.GroupID + "&RepoName=" + Group.RepoName
+	url := fmt.Sprintf(RepoURL, Group.GroupID)
 
 	val, ok := gr.Load(Group.RepoName)
 	if ok {
 		if interval > timer {
-			newbody, err := callAPI(c, url)
+			err := callAPI(c, url, &Repo)
 			if err != nil {
 				c.Error(err)
-				c.JSON(http.StatusBadGateway, gin.H{"status": http.StatusBadGateway})
+				c.JSON(http.StatusBadGateway, gin.H{"status": http.StatusBadGateway, "group_repos": val})
 				return
 			}
-			gr.Store(Group.RepoName, string(newbody))
+			gr.Store(Group.RepoName, Repo)
 			GroupTime = time.Now()
 
-			c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "GroupRepo": string(newbody)})
+			c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "group_repos": Repo})
 			return
 		}
 
-		c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "GroupRepo": val})
+		c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "group_repos": val})
 		return
 	}
 
-	body, err := callAPI(c, url)
+	err = callAPI(c, url, &Repo)
 	if err != nil {
 		c.Error(err)
 		c.JSON(http.StatusBadGateway, gin.H{"status": http.StatusBadGateway})
 		return
 	}
 
-	gr.Store(Group.RepoName, string(body))
+	gr.Store(Group.RepoName, Repo)
 	GroupTime = time.Now()
 
-	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "RepoList": string(body)})
+	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "group_repos": Repo})
 }
 
-func callAPI(c *gin.Context, url string) ([]byte, error) {
+func callAPI(c *gin.Context, url string, obj interface{}) error {
 	request, err := http.NewRequest("GET", url, nil)
+
 	if err != nil {
-		c.Error(err)
-		return nil, err
+		return err
 	}
 
 	Token := c.Request.Header
@@ -209,44 +212,14 @@ func callAPI(c *gin.Context, url string) ([]byte, error) {
 	client := &http.Client{}
 	response, err := client.Do(request)
 	if err != nil {
-		c.Error(err)
-		return nil, err
+		return err
 	}
 	defer response.Body.Close()
 
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		c.Error(err)
-		return nil, err
+		return err
 	}
 
-	check := bytes.Contains(body, []byte("ERROR"))
-	if check {
-		log.Println("Requst ERROR")
-		return nil, err
-	}
-
-	return body, nil
-}
-
-func handleDetail(body []byte, id string) {
-
-	var con = make([]string, 5)
-	con[0] = "封面"
-	con[1] = "作者"
-	con[2] = "内容简介"
-	con[3] = "捐助者"
-	con[4] = "时间"
-	info := bytes.Split(body, []byte("****"))
-
-	yuque = map[string]string{
-		con[0]: string(info[1]),
-		con[1]: string(info[3]),
-		con[2]: string(info[5]),
-		con[3]: string(info[7]),
-		con[4]: string(info[9]),
-	}
-	DetailTime = time.Now()
-	dr.Store(id, yuque)
-
+	return json.Unmarshal(body, obj)
 }
---
+++ model.go
@@ -0,0 +1,193 @@
+package main
+
+var (
+	// DetailURL -
+	DetailURL = "https://yuque.yangchengkai99.now.sh/api/v1/yuque/details?RepoID=%s&ID=%s"
+	// ListURL -
+	ListURL = "https://yuque.yangchengkai99.now.sh/api/v1/yuque/list?RepoID=%s"
+	// RepoURL -
+	RepoURL = "https://yuque.yangchengkai99.now.sh/api/v1/yuque/repo?GroupID=%s"
+)
+
+// ListRespon -
+type ListRespon struct {
+	List struct {
+		Data []struct {
+			ID                int    `json:"id"`
+			Slug              string `json:"slug"`
+			Title             string `json:"title"`
+			Description       string `json:"description"`
+			UserID            int    `json:"user_id"`
+			BookID            int    `json:"book_id"`
+			Format            string `json:"format"`
+			Public            int    `json:"public"`
+			Status            int    `json:"status"`
+			LikesCount        int    `json:"likes_count"`
+			CommentsCount     int    `json:"comments_count"`
+			ContentUpdatedAt  string `json:"content_updated_at"`
+			CreatedAt         string `json:"created_at"`
+			UpdatedAt         string `json:"updated_at"`
+			PublishedAt       string `json:"published_at"`
+			FirstPublishedAt  string `json:"first_published_at"`
+			DraftVersion      int    `json:"draft_version"`
+			LastEditorID      int    `json:"last_editor_id"`
+			WordCount         int    `json:"word_count"`
+			Cover             string `json:"cover"`
+			CustomDescription string `json:"custom_description"`
+			LastEditor        struct {
+				ID              int    `json:"id"`
+				Type            string `json:"type"`
+				Login           string `json:"login"`
+				Name            string `json:"name"`
+				Description     string `json:"description"`
+				AvatarURL       string `json:"avatar_url"`
+				LargeAvatarURL  string `json:"large_avatar_url"`
+				MediumAvatarURL string `json:"medium_avatar_url"`
+				SmallAvatarURL  string `json:"small_avatar_url"`
+				FollowersCount  int    `json:"followers_count"`
+				FollowingCount  int    `json:"following_count"`
+				CreatedAt       string `json:"created_at"`
+				UpdatedAt       string `json:"updated_at"`
+				Serializer      string `json:"_serializer"`
+			} `json:"last_editor"`
+			Book       string `json:"book"`
+			Serializer string `json:"_serializer"`
+		} `json:"data"`
+	} `json:"list"`
+	Status int `json:"status"`
+}
+
+// DetailRespon -
+type DetailRespon struct {
+	DetailData struct {
+		Abilities struct {
+			Update  bool `json:"update"`
+			Destroy bool `json:"destroy"`
+		} `json:"abilities"`
+		Data struct {
+			ID     int    `json:"id"`
+			Slug   string `json:"slug"`
+			Title  string `json:"title"`
+			BookID int    `json:"book_id"`
+			Book   struct {
+				ID               int    `json:"id"`
+				Type             string `json:"type"`
+				Slug             string `json:"slug"`
+				Name             string `json:"name"`
+				UserID           int    `json:"user_id"`
+				Description      string `json:"description"`
+				CreatorID        int    `json:"creator_id"`
+				Public           int    `json:"public"`
+				ItemsCount       int    `json:"items_count"`
+				LikesCount       int    `json:"likes_count"`
+				WatchesCount     int    `json:"watches_count"`
+				ContentUpdatedAt string `json:"content_updated_at"`
+				UpdatedAt        string `json:"updated_at"`
+				CreatedAt        string `json:"created_at"`
+				Namespace        string `json:"namespace"`
+				User             struct {
+					ID               int    `json:"id"`
+					Type             string `json:"type"`
+					Login            string `json:"login"`
+					Name             string `json:"name"`
+					Description      string `json:"description"`
+					AvatarURL        string `json:"avatar_url"`
+					LargeAvatarURL   string `json:"large_avatar_url"`
+					MediumAvatarURL  string `json:"medium_avatar_url"`
+					SmallAvatarURL   string `json:"small_avatar_url"`
+					BooksCount       int    `json:"books_count"`
+					PublicBooksCount int    `json:"public_books_count"`
+					FollowersCount   int    `json:"followers_count"`
+					FollowingCount   int    `json:"following_count"`
+					CreatedAt        string `json:"created_at"`
+					UpdatedAt        string `json:"updated_at"`
+					Serializer       string `json:"_serializer"`
+				} `json:"user"`
+				Serializer string `json:"_serializer"`
+			} `json:"book"`
+			UserID  int `json:"user_id"`
+			Creator struct {
+				ID               int    `json:"id"`
+				Type             string `json:"type"`
+				Login            string `json:"login"`
+				Name             string `json:"name"`
+				Description      string `json:"description"`
+				AvatarURL        string `json:"avatar_url"`
+				LargeAvatarURL   string `json:"large_avatar_url"`
+				MediumAvatarURL  string `json:"medium_avatar_url"`
+				SmallAvatarURL   string `json:"small_avatar_url"`
+				BooksCount       int    `json:"books_count"`
+				PublicBooksCount int    `json:"public_books_count"`
+				FollowersCount   int    `json:"followers_count"`
+				FollowingCount   int    `json:"following_count"`
+				CreatedAt        string `json:"created_at"`
+				UpdatedAt        string `json:"updated_at"`
+				Serializer       string `json:"_serializer"`
+			} `json:"creator"`
+			Format            string `json:"format"`
+			Body              string `json:"body"`
+			BodyDraft         string `json:"body_draft"`
+			BodyHTML          string `json:"body_html"`
+			BodyLake          string `json:"body_lake"`
+			Public            int    `json:"public"`
+			Status            int    `json:"status"`
+			LikesCount        int    `json:"likes_count"`
+			CommentsCount     int    `json:"comments_count"`
+			ContentUpdatedAt  string `json:"content_updated_at"`
+			DeletedAt         string `json:"deleted_at"`
+			CreatedAt         string `json:"created_at"`
+			UpdatedAt         string `json:"updated_at"`
+			PublishedAt       string `json:"published_at"`
+			FirstPublishedAt  string `json:"first_published_at"`
+			WordCount         int    `json:"word_count"`
+			Cover             string `json:"cover"`
+			Description       string `json:"description"`
+			CustomDescription string `json:"custom_description"`
+			Serializer        string `json:"_serializer"`
+		} `json:"data"`
+	} `json:"detail"`
+	Status int `json:"status"`
+}
+
+// RepoResp -
+type RepoResp struct {
+	Repo struct {
+		Data []struct {
+			ID               int    `json:"id"`
+			Type             string `json:"type"`
+			Slug             string `json:"slug"`
+			Name             string `json:"name"`
+			UserID           int    `json:"user_id"`
+			Description      string `json:"description"`
+			CreatorID        int    `json:"creator_id"`
+			Public           int    `json:"public"`
+			ItemsCount       int    `json:"items_count"`
+			LikesCount       int    `json:"likes_count"`
+			WatchesCount     int    `json:"watches_count"`
+			ContentUpdatedAt string `json:"content_updated_at"`
+			UpdatedAt        string `json:"updated_at"`
+			CreatedAt        string `json:"created_at"`
+			Namespace        string `json:"namespace"`
+			User             struct {
+				ID               int    `json:"id"`
+				Type             string `json:"type"`
+				Login            string `json:"login"`
+				Name             string `json:"name"`
+				Description      string `json:"description"`
+				AvatarURL        string `json:"avatar_url"`
+				LargeAvatarURL   string `json:"large_avatar_url"`
+				MediumAvatarURL  string `json:"medium_avatar_url"`
+				SmallAvatarURL   string `json:"small_avatar_url"`
+				BooksCount       int    `json:"books_count"`
+				PublicBooksCount int    `json:"public_books_count"`
+				FollowersCount   int    `json:"followers_count"`
+				FollowingCount   int    `json:"following_count"`
+				CreatedAt        string `json:"created_at"`
+				UpdatedAt        string `json:"updated_at"`
+				Serializer       string `json:"_serializer"`
+			} `json:"user"`
+			Serializer string `json:"_serializer"`
+		} `json:"data"`
+	} `json:"repo"`
+	Status int `json:"status"`
+}
